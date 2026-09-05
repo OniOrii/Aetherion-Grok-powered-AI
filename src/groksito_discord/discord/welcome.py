00ec22fbe109624f8e017a51fb9b50b7d50ac19d
Ori <onisyris@gmail.com>
2026-09-04 22:14:17 -0400

    Fix formatting in _banner function definition

--- src/groksito_discord/discord/welcome.py
+++ src/groksito_discord/discord/welcome.py
@@ -98,7 +98,7 @@ def _fill(template: str, member: discord.Member) -> str:
     return None
 
 
-async def _banner(member: discord.Member) -> discord.File | None:
+ async def _banner(member: discord.Member) -> discord.File | None:
     count = member.guild.member_count or 0
     avatar = member.display_avatar.replace(size=256).url
     bg = await _imagine_background(member)
