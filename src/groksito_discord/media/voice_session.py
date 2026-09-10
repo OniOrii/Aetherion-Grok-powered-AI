096607848f4dbc15f2e538bc5d6117185afdd438
Ori <onisyris@gmail.com>
2026-09-09 23:59:25 -0400

    Force Zagan on every voice session join.

--- src/groksito_discord/media/voice_session.py
+++ src/groksito_discord/media/voice_session.py
@@ -1,11 +1,22 @@
 from .voice_impl import (
-    DEFAULT_VOICE,
     DaveVoiceReceiver,
     VoiceSession,
     get_recv_cls,
-    start_session,
+    start_session as _start_session,
     stop_session,
 )
+
+DEFAULT_VOICE = "zagan"
+
+
+async def start_session(guild, voice_client, user_id, voice_name=None):
+    return await _start_session(
+        guild,
+        voice_client,
+        user_id,
+        voice_name=voice_name or DEFAULT_VOICE,
+    )
+
 
 __all__ = [
     "DEFAULT_VOICE",
